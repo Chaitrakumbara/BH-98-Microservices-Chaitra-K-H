ec0cb9db6afca7e6a80ca5d9df5758655502f8e2
Chaitrakumbara <chaitrakh011999@gmail.com>
2022-04-27 19:38:54 +0530

    27th April

--- Concepts/src/Java8/DateTime.java
+++ Concepts/src/Java8/DateTime.java
@@ -1,27 +1,28 @@
 package Java8;
 
 
-
-
 import java.time.LocalDate;
 import java.util.Scanner;
 
 
-
 public class DateTime {
 
-        public static void main(String[] args) {
-
-
+   public static void main(String[] args) {
 
        System.out.println("Enter a Number");
+       
        Scanner sc = new Scanner(System.in);
+       
        String day = sc.next();
+       
        LocalDate date = LocalDate.of(Integer.parseInt(day), 1, 1);
+       
        System.out.println(date.getDayOfWeek());
        System.out.println("Enter number of days you want to add ");
+       
        date = date.plusDays(sc.nextLong());
        sc.close();
+       
        System.out.println(date.getDayOfWeek());
 
 }
--- Concepts/src/Java8/SortingMonth.java
+++ Concepts/src/Java8/SortingMonth.java
@@ -9,48 +9,46 @@ package Java8;
 	public class SortingMonth {
 	public static void main(String[] args) {
 
-
-
-	Set<String> set = new TreeSet<String>(new Comparator<String>() {
-	public int compare(String o1, String o2) {
-	try {
-	SimpleDateFormat sdf = new SimpleDateFormat("MMM");
-	return sdf.parse(o1).compareTo(sdf.parse(o2)); //Compare Date with date
-	} catch (ParseException ex) {
-	return o1.compareTo(o2);
+	          Set<String> set = new TreeSet<String>(new Comparator<String>() {
+		
+	         public int compare(String o1, String o2) 
+	
+     	{
+		
+	        try
+	
+	     {
+		
+	       SimpleDateFormat sdf = new SimpleDateFormat("MMM");
+	       return sdf.parse(o1).compareTo(sdf.parse(o2)); 
+	
+	    } 
+	
+	        catch (ParseException ex) {
+	        return o1.compareTo(o2);
+	  }
 	}
-	}
-	});
+	  });
 
-	set.add("Feb");
-	set.add("Jan");
+	        set.add("Feb");
+	        set.add("Jan");
 
+	        set.add("Dec");
+	        set.add("Mar");
 
+	        set.add("June");
+	        set.add("May");
 
-	set.add("Dec");
-	set.add("Mar");
+	         set.add("Aug");
+	         set.add("July");
 
+	         set.add("Sep");
+	         set.add("Oct");
 
+	         set.add("April");
+	         set.add("Nov");
 
-	set.add("June");
-	set.add("May");
-
-
-
-	set.add("Aug");
-	set.add("July");
-
-
-
-	set.add("Sep");
-	set.add("Oct");
-
-	set.add("April");
-	set.add("Nov");
-
-	System.out.println(set);
-
-
+	        System.out.println(set);
 
 	}
 	}
