1bc7840338ae25548dd9b215ba108e77fb0ddfc5
Chaitrakumbara <chaitrakh011999@gmail.com>
2022-04-28 19:57:11 +0530

    28th April

--- examples/demo-Maven/demo-sboot/demo-sboot/src/main/java/com/example/demo/GradeController.java
+++ examples/demo-Maven/demo-sboot/demo-sboot/src/main/java/com/example/demo/GradeController.java
@@ -9,10 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GradeController {
 	
 	@RequestMapping("/Grade/{name}/{id}/{cmarks}/{pmarks}/{mmarks}")
-	String myStudents(@PathVariable String name, @PathVariable int id, @PathVariable int cmarks, @PathVariable int pmarks, @PathVariable int mmarks) {
+	
+	String myStudents(@PathVariable String name, @PathVariable int id, @PathVariable int cmarks, @PathVariable int pmarks, @PathVariable int mmarks)
+	
+	{
+		
 		StudentGrade s = new StudentGrade(id,name,pmarks,cmarks,mmarks);
 		String grade = s.getGrade();
 		return "Hi"+" "+name + " id " + id + " and your grade is " + grade; 
+		
 	}
 	
 	
--- examples/demo-Maven/demo-sboot/demo-sboot/src/main/java/com/mysite/main/DemoSbootApplication.java
+++ examples/demo-Maven/demo-sboot/demo-sboot/src/main/java/com/mysite/main/DemoSbootApplication.java
@@ -2,8 +2,13 @@ package com.mysite.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@ComponentScan("com.mysite.main")
+@EnableJpaRepositories("com.mysite.main")
 public class DemoSbootApplication {
 
 	public static void main(String[] args) {
